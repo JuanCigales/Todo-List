92bc153c8027c66dd171c2998d4f9b3d00dbcb2f
Juan Cigales <jcigales@192.168.1.121>
2024-07-17 16:57:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <Navbar />
+
+  <main class="container">
+    <Alert
+      message="Todo title is required"
+      :show="showAlert"
+      @close="showAlert = false"
+    />
+
+    <section>
+      <AddTodoForm @submit="addTodo" />
+    </section>
+
+    <section>
+      <Todo
+        v-for="todo in todos"
+        :key="todo.id"
+        :title="todo.title"
+        @remove="removeTodo(todo.id)"
+      />
+    </section>
+  </main>
+</template>
+
+<script>
+import Alert from "./components/Alert.vue";
+import Navbar from "./components/Navbar.vue";
+import AddTodoForm from "./components/AddTodoForm.vue";
+import Todo from "./components/Todo.vue";
+
+export default {
+  components: {
+    Alert,
+    Navbar,
+    AddTodoForm,
+    Todo,
+  },
+
+  data() {
+    return {
+      todoTitle: "",
+      todos: [],
+      showAlert: false,
+    };
+  },
+
+  methods: {
+    addTodo(title) {
+      if (title === "") {
+        this.showAlert = true; 
+        return;
+      }
+      this.todos.push({
+        title,
+        id: Math.floor(Math.random() * 1000),
+      });
+    },
+    removeTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+  },
+};
+</script>
