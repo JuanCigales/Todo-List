4122623fa5af2bdc456c8378457da9c3fb9d1270
Juan Cigales <jcigales@192.168.1.121>
2024-07-19 10:57:28 +0200

    EditTodoForm component refactor

--- src/components/EditTodoForm.vue
+++ src/components/EditTodoForm.vue
@@ -1,56 +1,54 @@
 <template>
-    <Modal :show="show" @close="$emit('close')">
-      <template #header>
-        <h2>Edit Todo</h2>
-      </template>
-  
-      <template #content>
-        <form class="edit-todo-form">
-          <div>
-            <label>Todo Title</label>
-          </div>
-          <input
-            type="text"
-            :value="modelValue"
-            @input="$emit('update:modelValue', $event.target.value)"
-          />
-        </form>
-      </template>
-  
-      <template #footer>
-        <div class="edit-todo-modal-footer">
-          <Btn class="edit-todo-submit-btn" @click="$emit('submit')">Submit</Btn>
-          <Btn variant="danger" @click="$emit('close')">Close</Btn>
+  <Modal :show="show" @close="$emit('close')">
+    <template #header>
+      <h2>Edit Todo</h2>
+    </template>
+
+    <template #content>
+      <form class="edit-todo-form">
+        <div>
+          <label>Todo Title</label>
         </div>
-      </template>
-    </Modal>
-  </template>
-  
-  <script>
-  import Modal from "./Modal.vue";
-  import Btn from "./Btn.vue";
-  export default {
-    components: {
-      Modal,
-      Btn,
-    },
-    props: ["modelValue", "show"],
-    emits: ["close", "submit", "update:modelValue"],
-  };
-  </script>
-  
-  <style scoped>
-  .edit-todo-form > input {
-    width: 100%;
-    height: 30px;
-    border: 1px solid var(--accent-color);
-  }
-  .edit-todo-modal-footer {
-    display: flex;
-    justify-content: end;
-    padding: 10px;
-  }
-  .edit-todo-submit-btn {
-    margin-right: 5px;
-  }
-  </style>
+        <input
+          type="text"
+          :value="modelValue"
+          @input="$emit('update:modelValue', $event.target.value)"
+        />
+      </form>
+    </template>
+
+    <template #footer>
+      <div class="edit-todo-modal-footer">
+        <Btn class="edit-todo-submit-btn" @click="$emit('submit')">Submit</Btn>
+        <Btn variant="danger" @click="$emit('close')">Close</Btn>
+      </div>
+    </template>
+  </Modal>
+</template>
+
+<script setup>
+import Modal from "./Modal.vue";
+import Btn from "./Btn.vue";
+
+defineProps(["modelValue", "show"]);
+
+defineEmits(["close", "submit", "update:modelvalue"]);
+</script>
+
+<style scoped>
+.edit-todo-form>input {
+  width: 100%;
+  height: 30px;
+  border: 1px solid var(--accent-color);
+}
+
+.edit-todo-modal-footer {
+  display: flex;
+  justify-content: end;
+  padding: 10px;
+}
+
+.edit-todo-submit-btn {
+  margin-right: 5px;
+}
+</style>
