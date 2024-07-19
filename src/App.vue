18c5bb30270b241eba170c0d2acfcc064dcc0e3d
Juan Cigales <jcigales@192.168.1.121>
2024-07-19 12:09:51 +0200

    useFetch

--- src/App.vue
+++ src/App.vue
@@ -30,16 +30,15 @@ import Todo from "./components/Todo.vue";
 import axios from "axios";
 import Spinner from "./components/Spinner.vue";
 import EditTodoForm from "./components/EditTodoForm.vue";
-import { reactive, ref } from "vue";
+import { reactive, ref, watch } from "vue";
+import { useFetch } from "./components/composables/fetch";
 
 
-const todos = ref([]);
 const alert = reactive({
   show: false,
   message: "",
   variant: "danger",
 });
-const isLoading = ref(false);
 const isPostingTodo = ref(false);
 const editTodoForm = reactive({
   show: false,
@@ -49,7 +48,9 @@ const editTodoForm = reactive({
   },
 });
 
-fetchTodos();
+const { data: todos, isLoading } = useFetch("/api/todos", {
+  onError: () => showAlert("Failed loading todos")
+});
 
 function showAlert(message, variant = "danger") {
   alert.show = true;
@@ -58,7 +59,7 @@ function showAlert(message, variant = "danger") {
 }
 
 function showEditTodoForm(todo) {
-  editTodoForm.show = true;
+  editTodoForm.show = true; 
   editTodoForm.todo = { ...todo };
 }
 
